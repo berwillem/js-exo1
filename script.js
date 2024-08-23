5ef70f5b1d6e5c5be266e6037f2437bea6791c70
berchiche willem <berwillempro@gmail.com>
2024-08-23 12:58:38 +0100

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function openSideBar() {
   document.querySelector(".sidebar").style.display = "flex";
 }
+
 function closeSideBar() {
   document.querySelector(".sidebar").style.display = "none";
 }
